6754f77fb94b38486aadab6800088f2f395f99bd
Luo Kun <luokun485@gmail.com>
2023-04-05 23:49:58 +0800

    .

--- generator.py
+++ generator.py
@@ -1,4 +1,4 @@
-from typing import List, Set
+from typing import List, Optional, OrderedDict
 
 import requests
 from yaml import safe_dump
@@ -24,47 +24,46 @@ proxy_domain_sets = [
     'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Ruleset/Porn.list',
     'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/ProxyGFWlist.list'
 ]
-
 direct_domain_sets = [
     'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/ChinaDomain.list',
 ]
 
-direct_domains = {
+direct_domains = OrderedDict.fromkeys([
     '+.satoko.life',
     'yacd.haishan.me',
     'clash.razord.top',
-}
-proxy_domains = {
+], True)
+proxy_domains = OrderedDict.fromkeys([
     'immersive-translate.owenyoung.com',
     '+.ea.com',
     'ea-api.arkoselabs.com',
     'origin-a.akamaihd.net',
     'eaassets-a.akamaihd.net',
-}
+], True)
 
 
-def generate(name: str, urls: List[str], domains: Set[str] = None):
-    domain_set, keyword_set = set(), set()
+def generate(name: str, urls: List[str], domains: Optional[OrderedDict[str, bool]] = None):
+    domain_set, keyword_set = OrderedDict(), OrderedDict()
     for url in urls:
         txt = requests.get(url).text
         for line in txt.splitlines():
             if line.startswith('DOMAIN-KEYWORD'):
-                keyword_set.add('DOMAIN-KEYWORD,' + line.split(',')[1])
+                keyword_set['DOMAIN-KEYWORD,' + line.split(',')[1]] = True
             elif line.startswith('DOMAIN-SUFFIX'):
-                domain_set.add('+.' + line.split(',')[1])
+                domain_set['+.' + line.split(',')[1]] = True
             elif line.startswith('DOMAIN'):
-                domain_set.add(line.split(',')[1])
+                domain_set[line.split(',')[1]] = True
 
     if domains is not None:
         domain_set |= domains
 
     if len(domain_set) > 0:
         with open(f'./providers/{name}-domains.yaml', 'w') as out:
-            safe_dump({'payload': list(domain_set)}, out)
+            safe_dump({'payload': list(domain_set.keys())}, out)
 
     if len(keyword_set) > 0:
         with open(f'./providers/{name}-keywords.yaml', 'w') as out:
-            safe_dump({'payload': list(keyword_set)}, out)
+            safe_dump({'payload': list(keyword_set.keys())}, out)
 
 
 if __name__ == "__main__":
